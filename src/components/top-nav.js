85d9a801dec1f05475cf994eb32eb6ae82a9ab3b
Cullan Bonilla <cullanbonilla@icloud.com>
2018-06-01 12:49:01 -0400

    implemented generateAuralUpdate action in reducer, connected relevant components

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import {restartGame} from '../actions';
+import {restartGame, generateAuralUpdate} from '../actions';
 
 import './top-nav.css';
 
@@ -34,6 +34,7 @@ export function TopNav(props) {
             /* the `visuallyhidden` class hides an element 
             while leaving it available to screen reader users  */
             className="visuallyhidden focusable status-link"
+            onClick={() => props.dispatch(generateAuralUpdate())}
           >
             Hear state of game
           </a>
